9b7b83ab8ee8e8c267d0ea6adb2108c512a8ff89
Florian Posdziech <hallo@flowfx.de>
2018-01-09 07:25:55 -0600

    Add list of movies

---
+++ entertainment_center.py
@@ -0,0 +1,32 @@
+import fresh_tomatoes
+import media
+
+toy_story = media.Movie(
+    'Toy Story',
+    'A story of a boy and its toys that come alive.',
+    'http://www.impawards.com/1995/posters/toy_story_ver1_xlg.jpg',
+    'https://www.youtube.com/watch?v=KYz2wyBy3kc',
+)
+
+avatar = media.Movie(
+    'Avatar',
+    'A marine on an alien planet.',
+    'http://www.impawards.com/2009/posters/avatar_xlg.jpg',
+    'https://www.youtube.com/watch?v=5PSNL1qE6VY',
+)
+
+blues_brothers = media.Movie(
+    'Blues Brothers',
+    'Two brothers bring the band back together.',
+    'poster image',
+    'youtube trailer',
+)
+
+the_commitments = media.Movie(
+    'The Commitments',
+    'An Irish soul band plays a concert without Wilson Picket.',
+    'poster image',
+    'youtune trailer',
+)
+
+movies = [toy_story, avatar, blues_brothers, the_commitments]
